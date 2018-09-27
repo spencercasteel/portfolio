a2e1c284364240550521e70953b6cf6b2666da42
Spencer Casteel <spencercasteel@interapts-macbook-pro-9.local>
2018-09-27 14:46:12 -0500

    added link to linkedIn

--- portfolioApp/ViewController.swift
+++ portfolioApp/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        self.view.backgroundColor = UIColor.cyan
     }
 
-
+    @IBAction func linkedIn(_ sender: Any) {
+        if let url = URL(string: "https://www.linkedin.com/in/spencer-casteel-16586b170/") {
+            UIApplication.shared.open(url, options: [:]) {
+                boolean in
+                
+            }
+        }
+    }
+    
 }
 
